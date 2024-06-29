3ddb5ddd891d6057f4430a118d823fbb1811ff9d
shin091602 <shin.soichiro.103@s.kyushu-u.ac.jp>
2024-06-29 14:21:05 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path =require('path');
+module.exports = {
+    mode: 'developmant',
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
